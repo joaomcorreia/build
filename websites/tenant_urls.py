f1f50bc4507780670c74e480f16f6e3a994c9c10
João Correia <joao-correia@outlook.com>
2025-10-22 20:36:34 +0200

    Add public URLs to tenant routing to fix wizard access

--- websites/tenant_urls.py
+++ websites/tenant_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'websites_tenant'
@@ -7,5 +7,7 @@ app_name = 'websites_tenant'
 urlpatterns = [
     path('', views.TenantHomeView.as_view(), name='home'),
     path('contact/', views.TenantContactView.as_view(), name='contact'),
+    # Include public account URLs for signup wizard access
+    path('', include('accounts.public_urls')),
     path('<slug:page_slug>/', views.TenantPageView.as_view(), name='page'),
 ]
